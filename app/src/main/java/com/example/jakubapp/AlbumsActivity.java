5eb1e02aa504af621f52fd22a045740bdf160be9
Jakub Ryszka <ryszkajakub6@gmail.com>
2022-10-04 22:52:40 +0200

    add and remove directory

--- app/src/main/java/com/example/jakubapp/AlbumsActivity.java
+++ app/src/main/java/com/example/jakubapp/AlbumsActivity.java
@@ -2,9 +2,10 @@ package com.example.jakubapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.view.View;
 
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -23,11 +25,29 @@ public class AlbumsActivity extends AppCompatActivity {
     private ListView listView;
     private LinearLayout addFolderBtn;
 
+    public void refreshDirectory(File dir){
+
+        String[] array = new String[dir.listFiles().length];
+        for(int i=0; i< dir.listFiles().length; i++){
+            array[i] = dir.listFiles()[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                AlbumsActivity.this,
+                R.layout.list_view_elem,
+                R.id.tv1,
+                array
+        );
+        listView.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
+
+
+
 
         File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File dir = new File(pic, "RyszkaJakub");
@@ -47,19 +67,73 @@ public class AlbumsActivity extends AppCompatActivity {
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-
         listView = findViewById(R.id.listViewID);
-        String[] array = new String[dir.listFiles().length];
-        for(int i=0; i< dir.listFiles().length; i++){
-            array[i] = dir.listFiles()[i].getName();
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                AlbumsActivity.this,
-                R.layout.list_view_elem,
-                R.id.tv1,
-                array
-        );
-        listView.setAdapter(adapter);
+        refreshDirectory(dir);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("longClikc", adapterView.getItemAtPosition(i).toString());
+                AlertDialog.Builder alert = new AlertDialog.Builder(AlbumsActivity.this);
+                alert.setTitle("USUWANIE FOLDERU");
+                alert.setMessage("Czy na pewno usunać?");
+                alert.setPositiveButton("USUŃ", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        File toRemove = new File(pic, "RyszkaJakub/" + adapterView.getItemAtPosition(i).toString());
+                        toRemove.delete();
+
+                    }
+                });
+                alert.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                    }
+                });
+                alert.show();
+                return false;
+            }
+        });
+
+
+
+
+
+        addFolderBtn = findViewById(R.id.addButton);
+        addFolderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(AlbumsActivity.this);
+                alert.setTitle("Nowy folder");
+                alert.setMessage("Podaj nazwe nowego folderu");
+                EditText input = new EditText(AlbumsActivity.this);
+                input.setText("Nowy folder");
+                alert.setView(input);
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                        File dir = new File(pic, "RyszkaJakub/" +  input.getText());
+                        dir.mkdir();
+
+
+                    }
+                });
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                    }
+                });
+
+                alert.show();
+            }
+
+
+        });
+
+
 
 
 
