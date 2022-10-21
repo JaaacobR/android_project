9b24e3fa078c18da1b76aef652081e0f184d1670
Jakub Ryszka <ryszkajakub6@gmail.com>
2022-10-21 12:14:04 +0200

    add constructor to arrayAdpater

--- app/src/main/java/com/example/jakubapp/TestAdapter.java
+++ app/src/main/java/com/example/jakubapp/TestAdapter.java
@@ -1,4 +1,22 @@
 package com.example.jakubapp;
 
-public class TestAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAdapter extends ArrayAdapter {
+
+
+    public TestAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    private ArrayList<String> _list;
+    private Context _context;
+    private int resource;
+
 }
