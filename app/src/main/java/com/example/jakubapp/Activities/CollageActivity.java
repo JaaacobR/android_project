dd9b04ad43621180012aae328d6a293ba1076a6a
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-01-20 11:47:30 +0100

    add layout

--- app/src/main/java/com/example/jakubapp/Activities/CollageActivity.java
+++ app/src/main/java/com/example/jakubapp/Activities/CollageActivity.java
@@ -3,6 +3,7 @@ package com.example.jakubapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.jakubapp.ImageData;
 import com.example.jakubapp.R;
@@ -12,11 +13,18 @@ import java.util.ArrayList;
 public class CollageActivity extends AppCompatActivity {
 
     private ArrayList<ImageData> imageDataArrayList = new ArrayList<>();
+    private LinearLayout collageFirst;
+    private LinearLayout collageSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collage);
+
+        collageFirst = findViewById(R.id.collageFirst);
+        collageSecond = findViewById(R.id.collageSecond);
+
+        imageDataArrayList.add(new ImageData(0,0,100,100));
 
     }
 }
